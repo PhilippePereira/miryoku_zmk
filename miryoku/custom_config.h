af4376766abc2962627adfa0daf3baad63d4047d
PhilippePereira <61878453+PhilippePereira@users.noreply.github.com>
2026-03-20 19:17:04 +0100

    Update custom_config.h
    custom nav layer ordre qui respecte l'original + suppression QMK standard

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define MIRYOKU_LAYER_NAV \
-U_BOOT,    &u_tap_tap, U_NA,      U_NA,      U_NA,           &kp INS,   &kp HOME,  &kp PG_DN, &kp PG_UP, &kp END, \
-&kp LGUI,  &kp LALT,   &kp LCTRL, &kp LSHFT, U_NA,           &kp LEFT,  &kp DOWN,  &kp UP,    &kp RIGHT, &kp CAPS,  \
-U_NA,      &kp RALT,   &u_num_num,&u_nav_nav,U_NA,           U_NA,      &u_cut,    &u_cpy,    &u_pst,    &u_und, \
-U_NP,      U_NP,       U_NA,      U_NA,      U_NA,           &kp RET,   &kp BSPC,  &kp DEL,   U_NP,      U_NP
+U_BOOT,    U_TAP,     U_NA,      U_NA,      U_NA,           U_RDO,   U_PST,   U_CPY,   U_CUT,    U_UND, \
+KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   U_NA,           KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_CAPS, \
+U_NA,      KC_RALT,   U_NUM,     U_NAV,     U_NA,           KC_INS,     KC_HOME,     KC_PGDN,     KC_PGUP,     KC_END, \
+U_NP,      U_NP,      U_NA,      U_NA,      U_NA,           KC_ENT,    KC_BSPC,   KC_DEL,    U_NP,      U_NP
