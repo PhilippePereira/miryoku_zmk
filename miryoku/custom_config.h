617c089110ec0a9e493dbc5fa6f9f2716b216e8f
PhilippePereira <61878453+PhilippePereira@users.noreply.github.com>
2026-03-20 19:26:27 +0100

    Update custom_config.h
    cancel custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-#pragma once
-
-#define MIRYOKU_LAYER_NAV \
-U_BOOT,    U_TAP,     U_NA,      U_NA,      U_NA,           U_RDO,    U_PST,     U_CPY,     U_CUT,     U_UND, \
-KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   U_NA,           KC_LEFT,  KC_DOWN,   KC_UP,     KC_RGHT,   KC_CAPS, \
-U_NA,      KC_RALT,   U_NUM,     U_NAV,     U_NA,           KC_INS,   KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END, \
-U_NP,      U_NP,      U_NA,      U_NA,      U_NA,           KC_ENT,   KC_BSPC,   KC_DEL,    U_NP,      U_NP
