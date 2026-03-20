ffbc43ca1d70f13277a21e1eb07330c7bf89d7a9
PhilippePereira <61878453+PhilippePereira@users.noreply.github.com>
2026-03-20 18:51:51 +0100

    Update custom_config.h
    Custom nav layer avec les touches décalées

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#pragma once
+
+#define MIRYOKU_LAYER_NAV \
+U_BOOT,    &u_tap_tap, U_NA,      U_NA,      U_NA,           &kp INS,   &kp HOME,  &kp PG_DN, &kp PG_UP, &kp END, \
+&kp LGUI,  &kp LALT,   &kp LCTRL, &kp LSHFT, U_NA,           &kp LEFT,  &kp DOWN,  &kp UP,    &kp RIGHT, &kp CAPS,  \
+U_NA,      &kp RALT,   &u_num_num,&u_nav_nav,U_NA,           U_NA,      &u_cut,    &u_cpy,    &u_pst,    &u_und, \
+U_NP,      U_NP,       U_NA,      U_NA,      U_NA,           &kp RET,   &kp BSPC,  &kp DEL,   U_NP,      U_NP
